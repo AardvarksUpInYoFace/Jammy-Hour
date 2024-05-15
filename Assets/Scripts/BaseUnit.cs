c19ce764746cc665eadd9199f4096e1b41be2687
AardvarksUpInYoFace <jonathanjacob_L@hotmail.com>
2024-05-15 21:36:57 +0200

    Healthbar

--- Assets/Scripts/BaseUnit.cs
+++ Assets/Scripts/BaseUnit.cs
@@ -5,11 +5,22 @@ public class BaseUnit : MonoBehaviour
     public bool IsEnemy;
     public int Health;
 
+    [HideInInspector]
+    public int maxHealth;
+
+    public System.Action OnChangeHealth;
     public System.Action<BaseUnit> OnDie;
+
+    void Start()
+    {
+        maxHealth = Health;
+    }
 
     public void ChangeHealth(int change)
     {
         Health += change;
+
+        OnChangeHealth?.Invoke();
 
         if (Health <= 0)
             Die();
--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -1,8 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour
 {
-    
+    public BaseUnit unit;
+    public Image image;
+
+    void Start()
+    {
+        if (unit == null)
+            unit = transform.parent.parent.GetComponent<BaseUnit>();
+
+        unit.OnChangeHealth += ChangingHealth;
+    }
+
+    void ChangingHealth()
+    {
+        image.fillAmount = (float)unit.Health / unit.maxHealth;
+    }
 }
